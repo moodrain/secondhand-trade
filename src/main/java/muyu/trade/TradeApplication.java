409aa30e27cdc0746bb23149d7747c9a65761951
moodrain <moerain@qq.com>
2020-01-18 08:20:53 +0800

    simplify code

--- src/main/java/muyu/trade/TradeApplication.java
+++ src/main/java/muyu/trade/TradeApplication.java
@@ -1,12 +1,7 @@
 package muyu.trade;
 
-import muyu.trade.util.HttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.HashMap;
 
 @SpringBootApplication
 public class TradeApplication {
